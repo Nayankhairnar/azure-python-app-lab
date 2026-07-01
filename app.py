54d89452c61ec3922b8a21aead1d032ded3a9dc0
Nayan Khairnar <nayankhairnar9168@gmail.com>
2026-07-01 09:18:12 +0200

    Fix exclamation marks in home route response

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Python App deployed via GitHub CI/CD successfully!"
+    return "✅ Python App deployed via GitHub CI/CD successfully!!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
